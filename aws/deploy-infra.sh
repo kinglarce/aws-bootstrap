f1a11a710f9e21a675441f8464a9cc4a8ba7c624
King Larce Cortes <kingcort01@gmail.com>
2022-02-07 15:02:48 +0800

    Split out stack and add packaging

--- aws/deploy-infra.sh
+++ aws/deploy-infra.sh
@@ -15,7 +15,8 @@ CLOUDFORMATION_BUCKET="$STACK_NAME-$REGION-cfn-$AWS_ACCOUNT_ID"
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID"
 
 deploy_setup_stack() {
-    echo "deploying setup stack..."
+    echo -e "\n\n=========== Deploying setup.yml ==========="
+    echo "Deploying static resources setup stack..."
 
     aws cloudformation deploy \
         --region $REGION \
@@ -25,21 +26,48 @@ deploy_setup_stack() {
         --no-fail-on-empty-changeset \
         --capabilities CAPABILITY_NAMED_IAM \
         --parameter-overrides \
-            CodePipelineBucket=$CODEPIPELINE_BUCKET
+            CodePipelineBucket=$CODEPIPELINE_BUCKET \
+            CloudFormationBucket=$CLOUDFORMATION_BUCKET 
+}
+
+deploy_package_stack() {
+    # Package up stack templates, uploading nested stacks to s3 (as required
+    # by CloudFormation)
+    echo -e "\n\n=========== Packaging main.yml ==========="
+    echo "Packaging stack templates..."
+
+    mkdir -p ./cfn_output
+
+    PACKAGE_ERR="$(aws cloudformation package \
+        --region $REGION \
+        --profile $AWS_CLI_PROFILE \
+        --template main.yml \
+        --s3-bucket $CLOUDFORMATION_BUCKET \
+        --output-template-file ./cfn_output/main.yml 2>&1)"
+
+    if ! [[ $PACKAGE_ERR =~ "Successfully packaged artifacts" ]]; then
+        echo "ERROR while running 'aws cloudformation package' command:"
+        echo $PACKAGE_ERR
+        exit 1
+    fi
 }
 
 deploy_stack() {
     deploy_setup_stack
+    deploy_package_stack
+    # Deploy the CloudFormation template
+    echo -e "\n\n=========== Deploying main.yml ==========="
     echo "deploying stack..."
 
     aws cloudformation deploy \
       --region $REGION \
       --profile $AWS_CLI_PROFILE \
       --stack-name $STACK_NAME \
-      --template-file main.yml \
+      --template-file ./cfn_output/main.yml \
       --no-fail-on-empty-changeset \
       --capabilities CAPABILITY_NAMED_IAM \
-      --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE \
+      --parameter-overrides \
+        EC2InstanceType=$EC2_INSTANCE_TYPE \
         GitHubOwner=$GITHUB_OWNER \
         GitHubRepo=$GITHUB_REPO \
         GitHubBranch=$GITHUB_BRANCH \
@@ -48,9 +76,9 @@ deploy_stack() {
 
     # If the deploy succeeded, show the DNS name of the created instance
     if [ $? -eq 0 ]; then
-      aws cloudformation list-exports \
-        --profile awsbootstrap \
-        --query "Exports[?starts_with(Name,'LBEndpoint')].Value"
+        aws cloudformation list-exports \
+            --profile $AWS_CLI_PROFILE \
+            --query "Exports[?ends_with(Name,'LBEndpoint')].Value"
     fi
 }
 
